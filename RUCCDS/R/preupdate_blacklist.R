ec99c2e0e50b1ab71295f3866d8ef3a3b471a6d0
LiuTX-CH <69201866+LiuTX-CH@users.noreply.github.com>
2022-09-24 22:01:01 +0800

    Add files via upload

--- RUCCDS/R/preupdate_blacklist.R
+++ RUCCDS/R/preupdate_blacklist.R
@@ -1,7 +1,7 @@
 #' @title Update penalty blacklist before the lecture
-#' @description To update the penalty blacklist when someone had been punished（removed from the enrollment meal.notice）
+#' @description To update the penalty blacklist when someone had been punished（removed from the meal.notice）
 #' @param blacklist The CDS blacklist: An object read by readxl package
-#' @param meal.notice Enrollment meal.notice of CDS lectures: An object read by readxl package
+#' @param meal.notice meal.notice of CDS lectures: An object read by readxl package
 #' @return Updated blacklist
 #' @export
 #'
--- RUCCDS/R/search_blacklist.R
+++ RUCCDS/R/search_blacklist.R
@@ -1,7 +1,7 @@
 #' @title Search blacklist and update meal.notice
-#' @description To remove names on the blacklist from the meal.notice. There is no one need to be removed if no object returned.
+#' @description To remove names on the blacklist from the meal.notice.
 #' @param blacklist The CDS blacklist: An object read by readxl package
-#' @param meal.notice Enrollment meal.notice of CDS lectures: An object read by readxl package
+#' @param meal.notice meal.notice of CDS lectures: An object read by readxl package
 #' @return Updated meal.notice
 #' @export
 #'
@@ -12,12 +12,16 @@ search_blacklist <- function(blacklist, meal.notice){
   name.list <- 1:2
   for(j in 1:nrow(meal.notice)){
     if(meal.notice[j, 1]%in%pen$name == T){
-      meal.notice <- meal.notice[-j, ]
+      newmeal.notice <- meal.notice[-j, ]
       name.list <- append(name.list, meal.notice[j, 1])
     }
   }
   name.list <- name.list[-c(1,2)]
+  meal.notice <- meal.notice[!(meal.notice$name%in%name.list),]
   cat(paste0(c("下列人员需要通知取消用餐资格一次：",name.list),"\n",collapse = " "))
-  cat(paste0("新名单需要递补",length(name.list),"人"))
+  if (!(length(name.list))){
+    cat("该名单可用，无需递补")
+  } else cat(paste0("新名单需要递补",length(name.list),"人"))
+
   return(meal.notice)
 }
